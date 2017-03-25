4f46d8e6a41f26af26af58d0111b8e60d01454c3
EvgenijSemenov <semenovevgenij92@gmail.com>
2017-03-25 20:52:41 +0200

    Add routes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GoodListComponent } from './good-list/good-list.component';
+import {CarService} from "./service/car.service";
+import { AddGoodComponent } from './add-advertisement/add-advertisement.component';
+import { RouterModule }   from '@angular/router';
+import { EditGoodComponent } from './edit-good/edit-good.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoodListComponent,
+    AddGoodComponent,
+    EditGoodComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: GoodListComponent
+      },
+      {
+        path: 'add-good',
+        component: AddGoodComponent
+      },
+      {
+        path: 'edit-good/:id',
+        component: EditGoodComponent
+      }
+    ])
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CarService],
+  bootstrap: [AppComponent],
+
 })
+
 export class AppModule { }
