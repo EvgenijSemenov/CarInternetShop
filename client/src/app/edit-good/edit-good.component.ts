386721029763cd209a97495d0588efcf926277fe
EvgenijSemenov <semenovevgenij92@gmail.com>
2017-03-25 20:51:22 +0200

    Add EditGoodComponent

---
+++ client/src/app/edit-good/edit-good.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute} from "@angular/router";
+import {Car} from "../entity/car";
+import {CarService} from "../service/car.service";
+
+@Component({
+  selector: 'app-edit-good',
+  templateUrl: './edit-good.component.html'
+})
+
+export class EditGoodComponent implements OnInit {
+
+  private car = new Car(null);
+  private errorMessage: any;
+
+  constructor(private carService: CarService, private route: ActivatedRoute,private router: Router) {}
+
+  ngOnInit() {
+    let routeId = this.route.snapshot.params['id'];
+
+    this.carService.getCar(routeId).subscribe(
+      car => this.car = car[0],
+      error =>  this.errorMessage = <any>error
+    );
+  }
+
+  updateCar() {
+    this.carService.updateCar(this.car).subscribe(
+       car => this.router.navigate(['']),
+       error =>  this.errorMessage = <any>error
+     );
+  }
+
+}
