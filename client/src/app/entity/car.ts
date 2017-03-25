f6673d7828d253fe5283557188c38c9d132a2c6c
EvgenijSemenov <semenovevgenij92@gmail.com>
2017-03-25 20:46:55 +0200

    Create car class

---
+++ client/src/app/entity/car.ts
@@ -0,0 +1,14 @@
+export class Car {
+
+  constructor(
+    public id: number,
+    public status?: string,
+    public model?: string,
+    public age?: number,
+    public race?: number,
+    public fuel_type?: string,
+    public description?: string,
+    public price?: number
+  ) {}
+
+}
