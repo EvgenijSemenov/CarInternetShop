4b24d1f0b7672b28a6504f95ec4c8d6fb2075ec7
EvgenijSemenov <semenovevgenij92@gmail.com>
2017-03-30 18:33:24 +0300

    Create method registrateNewUser

--- client/src/app/registration/registration.component.ts
+++ client/src/app/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Md5} from 'ts-md5/dist/md5';
+import {User} from "../entity/user";
+import {UserService} from "../service/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  private user: User = new User(null, "", "", "", "");
+  private password: string = "";
+  private errorMessage: any;
+
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  registrateNewUser(){
+    this.user.pass_hash = Md5.hashStr(this.user.email + ":" + this.password).toString();
+    this.userService.addUser(this.user).subscribe(
+      user => this.router.navigate(['']),
+      error =>  this.errorMessage = <any>error
+    );
   }
 
 }
