6e2f9e2bfddd909166a132c52d02a9bd719f5b42
EvgenijSemenov <semenovevgenij92@gmail.com>
2017-04-01 14:09:32 +0300

    Add header and show only index page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  title = 'Інтернет магазин автомобілів';
+
+  public isAuthUser: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    let authToken = localStorage.getItem('auth-token');
+    console.log(this.router.url);
+  }
 }
